1c8bf73585c8fb5ae3c7b09780356b3382b06342
Gopal Mishra <gopalmishra@live.com>
2020-04-05 21:42:59 +0530

    Added Angular material and created basic components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ActionBarComponent } from './base-components/action-bar/action-bar.component';
+import { SideBarComponent } from './base-components/side-bar/side-bar.component';
+import { NotesEditComponent } from './base-components/notes-edit/notes-edit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ActionBarComponent,
+    SideBarComponent,
+    NotesEditComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/base-components/notes-edit/notes-edit.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-notes-edit',
+  templateUrl: './notes-edit.component.html',
+  styleUrls: ['./notes-edit.component.scss']
+})
+export class NotesEditComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
